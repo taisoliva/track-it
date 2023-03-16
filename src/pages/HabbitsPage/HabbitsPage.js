d84b740b28f8cb894842900a9f9749c5d97b94ba
taisoliva <tais15oliva@gmail.com>
2023-03-16 16:55:23 -0300

    Create a Habit

--- src/pages/HabbitsPage/HabbitsPage.js
+++ src/pages/HabbitsPage/HabbitsPage.js
@@ -3,12 +3,24 @@ import LogoTrackit from "../../assets/TrackIt.png"
 import { useEffect, useState } from "react"
 import { URL_HABITS } from "../Urls"
 import axios from "axios"
-import { Content, ContentFixed, ContainerIonIcon, Text } from "./styled"
+import {
+    Content, ContentFixed, ContainerIonIcon,
+    Text, NewHabit, Days, Week, ButtonSave, ButtonCancel
+} from "./styled"
 import { Link } from "react-router-dom"
+import { Input } from "../styledCommon"
+import Day from "../../components/Day"
+import { ThreeDots } from "react-loader-spinner"
+
 
 export default function HabbitsPage({ token }) {
 
     const [dataHabit, setDataHabit] = useState("")
+    const [disabledButton, setDisabledButton] = useState(false)
+    const [form, setForm] = useState({ name: "", days: "" })
+    const [ativeAdd, setAtiveAdd] = useState("none")
+    const [selectedDays, setSelectedDays] = useState([]);
+    const week = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
 
     useEffect(() => {
         const config = {
@@ -21,6 +33,55 @@ export default function HabbitsPage({ token }) {
 
     }, [])
 
+    function addHabit() {
+        setAtiveAdd("flex")
+    }
+
+    function cancel (){
+        setAtiveAdd("none")
+        setDisabledButton(false)
+    }
+
+    function handleForm(event) {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    function handleDaySelect(day) {
+        const array = [...selectedDays]
+        if (array.includes(day)) {
+            array.filter(d => d !== day);
+            setSelectedDays(array)
+        } else {
+            array.push(day)
+            setSelectedDays(array)
+        }
+
+        setForm({...form, days: array})
+    }
+
+    function Save (event){
+        event.preventDefault()
+        
+        setDisabledButton(true)
+        const body = { ...form }
+       
+        console.log(body)
+
+        const config ={
+            headers : {Authorization : `Bearer ${token}`}
+        }
+        
+        axios.post(URL_HABITS, body ,config)
+        .then(res =>{console.log(res)
+                     setDisabledButton(false)
+                     setSelectedDays([])
+                     setAtiveAdd("none")
+                     setForm({ name: "", days: "" })
+        } )
+        .catch(err => {alert(err.response.data.message); setDisabledButton(false)})
+
+    }
+
     return (
 
         <Container>
@@ -32,13 +93,42 @@ export default function HabbitsPage({ token }) {
             <Content>
                 <ContentFixed>
                     <p> Meus Hábitos </p>
-                    <ContainerIonIcon>
+                    <ContainerIonIcon data-test="habit-create-btn" onClick={addHabit}>
                         <ion-icon name="add-outline"></ion-icon>
                     </ContainerIonIcon>
                 </ContentFixed>
 
-                {dataHabit.length === 0 && <Text> Você não tem nenhum hábito cadastrado ainda. 
-                                                   Adicione um hábito para começar a trackear!  </Text>}
+                <NewHabit data-test="habit-create-container" ativeAdd={ativeAdd} onSubmit={Save}>
+                    <form>
+                        <Input data-test="habit-name-input"
+                            type="text"
+                            placeholder="nome do hábito"
+                            name={"name"}
+                            value={form.name}
+                            disabled={disabledButton}
+                            onChange={handleForm} />
+
+                        <Week>
+                            {week.map((d,indice) => <Day key={indice}
+                                                handleDaySelect={handleDaySelect}
+                                                d={d}
+                                                indice={indice}
+                                                disabledButton={disabledButton}
+                                                selectedDays={selectedDays} />)}
+                        </Week>
+
+                        <ButtonCancel data-test="habit-create-cancel-btn" type="button" onClick={cancel}> Cancelar </ButtonCancel>
+                        <ButtonSave data-test="habit-create-save-btn" type="submit" disabled={disabledButton} > {disabledButton ? <ThreeDots color="#FFFFFF" height={80} width={80} timeout={3000} // 3 secs 
+                        /> : "Salvar"
+                        } </ButtonSave>
+
+                        <Text> Você não tem nenhum hábito cadastrado ainda.
+                            Adicione um hábito para começar a trackear!
+                        </Text>
+                    </form>
+                </NewHabit>
+
+
             </Content>
 
             <Footer data-test="menu">
--- src/pages/HabbitsPage/styled.js
+++ src/pages/HabbitsPage/styled.js
@@ -7,7 +7,7 @@ export const Content = styled.div`
 
     
 
-    position: absolute;
+    position: relative;
 
     top:100px;
     display: flex;
@@ -20,8 +20,6 @@ export const ContentFixed = styled.div`
     width: 90vw;
     height: 35px;
 
-    
-    
     position: absolute;
 
     
@@ -62,16 +60,15 @@ export const ContainerIonIcon = styled.div`
 `
 export const Text = styled.div`
     width: 90vw;
-    height: 35px;
+    height: 70px;
 
+    margin: 7vh auto;
     
     
+
     position: absolute;
-
-    margin-top: 60px;
-
-    left: 20px;
-
+    left: 0;
+   
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
@@ -83,5 +80,106 @@ export const Text = styled.div`
     display: flex;
     flex-wrap: wrap;
 
+    
+`
+
+export const NewHabit = styled.div`
+
+    width: 90vw;
+    height: 180px;  
+
+    background-color: #FFFFFF;
+
+    margin-top: 100px;
+    margin: 7vh auto;
+
+    border-radius: 5px;
+
+    display: ${props => props.ativeAdd};
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 2vh;
+
+    position: relative;
+    
+`
+
+export const Week = styled.div`
+    display: flex;
+
+    margin-bottom: 30px;
+    
+`
+
+export const Days = styled.button`
+
+    width: 30px;
+    height: 30px;
+
+    background-color: ${props => props.color ? "#CFCFCF" : "#FFFFFF"};
+
+    border: none;
+
+    margin-right: 5px;
+
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19.976px;
+    line-height: 25px;
+
+    color: #DBDBDB;
+`
+export const ButtonSave = styled.button`
+    width: 90px;
+    height: 35px;
+    background-color: #52B6FF;
+    border-radius: 5px;
+    border:none;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    text-align: center;
+    color: #FFFFFF;
+
+    position: absolute;
+
+    bottom: 15px;
+    right: 15px;
+    
+`
+
+export const ButtonCancel = styled.button`
+    width: 90px;
+    height: 35px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    border:none;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    text-align: center;
+    color: #52B6FF;
+
+    position: absolute;
+
+    bottom: 15px;
+    right: 120px;
     
 `
