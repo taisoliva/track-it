1002538e590e0d7741e15e152bbd50f1808a7ce4
taisoliva <tais15oliva@gmail.com>
2023-03-19 20:32:18 -0300

    Put a overflow

--- src/pages/styledCommon.js
+++ src/pages/styledCommon.js
@@ -94,11 +94,13 @@ export const ImageUser = styled.div`
 export const Footer = styled.div`
     position: fixed;
 
+    z-index: 1;
     bottom: 0;
 
     width: 100%;
     height: 70px;
 
+    background-color: #FFFFFF;
     display: flex;
     align-items: center;
     justify-content: space-between;
